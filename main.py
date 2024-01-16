d5e1bb9fc4b12a6ba291253f2dbab2ae9635cbfb
Szöllősi Bálint (SZF_2023_2025) <szollosi.balint@diak.szamalk-szalezi.hu>
2024-01-16 10:10:13 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+i=1
+n=0
+ossz=0
+while :
+    print("N=")
+    n=int(input("Adjon meg egy számot"))
+while n<0:
+    n = int(input("Adjon meg egy számot"))
+
+
+while i<=n:
+    ossz+=0
+    i+=1
+print(f"Az ellső {n} db t. szám összege")
